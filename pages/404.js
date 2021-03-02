b75b0c8449c59d2199671e76e2747262b743d072
Emmanuel Roussel <emmanuel@emroussel.com>
2021-03-01 19:40:11 -0500

    First commit

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+export default function NotFound({ content }) {
+  return (
+    <h1>{content.title}</h1>
+  );
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      content: { title: "404" },
+    },
+  };
+}
